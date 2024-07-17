e932040cd31dad81f11c8b8427e5f47b0f00bc40
Muniexe <edmz112306@gmail.com>
2024-07-17 11:26:58 -0300

    Horas Trabalhadas

---
+++ Horas Trabalhadas.py
@@ -0,0 +1,5 @@
+Salário = float(input("Qual o seu Salário?: "))
+Horas = int(input("Quantas Horas você Trablha No Mês?: "))
+dinheiroporhora = int(Salário/Horas)
+
+print ("Seu Dinheiro por hora é: ",dinheiroporhora,"R$ ""Hora")
